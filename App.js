8b7a8598ae1698d7b13f1e292da8a5d2c8c0b7ac
Parthkamble1 <87113973+Parthkamble1@users.noreply.github.com>
2022-05-23 09:02:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import BottomTabNavigator from './components/bottomTabNavigator';
+
+export default class App extends Component {
+  render(){
+    return (
+      <BottomTabNavigator/>
+    );
+  }
+  
+}
+
+/*const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});*/
